f24b446b4303546a31bbbdae3304a7be017854cd
Kyle Upton <kyleaupton@gmail.com>
2023-10-20 14:43:22 -0400

    Initial Commit

---
+++ Lush Schedule Helper/Lush_Schedule_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lush_Schedule_HelperApp.swift
+//  Lush Schedule Helper
+//
+//  Created by Kyle Upton on 10/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Lush_Schedule_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
